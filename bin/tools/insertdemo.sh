d1dc98c62784ffd218a3cae1eb2aad4edc37067f
xrow <bjoern@xrow.de>
2014-12-10 21:11:59 +0100

    - fix insertdemo.sh

--- bin/tools/insertdemo.sh
+++ bin/tools/insertdemo.sh
@@ -6,10 +6,19 @@ else
     mysqlauth=" -h ${DATABASE_SERVER} -u ${DATABASE_USER} -p${DATABASE_PASSWORD} " 
 fi
 rm -Rf ezpublish_legacy/settings/override ezpublish_legacy/settings/siteaccess ezpublish_legacy/var/ezdemo_site/log ezpublish_legacy/var/ezdemo_site/cache
-wget --no-check-certificate -O dump.zip https://github.com/xrowgmbh/xrowvagrant-demodata/archive/master.zip
-unzip -o -d $PWD dump.zip
-cp -r xrowvagrant-demodata-master/* .
-rm -Rf xrowvagrant-demodata-master
+
+if [ ! ${VERSION} ]; then
+  wget --no-check-certificate -O dump.zip https://github.com/xrowgmbh/xrowvagrant-demodata/archive/master.zip
+  unzip -o -d $PWD dump.zip
+  cp -r xrowvagrant-demodata-master/* .
+  rm -Rf xrowvagrant-demodata-master
+else
+  wget --no-check-certificate -O dump.zip https://github.com/xrowgmbh/xrowvagrant-demodata/archive/${VERSION}.zip
+  unzip -o -d $PWD dump.zip
+  cp -r xrowvagrant-demodata-${VERSION}/* .
+  rm -Rf xrowvagrant-demodata-${VERSION}
+fi
+
 rm -Rf ezpublish_legacy/var/ezdemo_site/log/ ezpublish_legacy/var/ezdemo_site/cache/
 mysql ${mysqlauth} -e"CREATE DATABASE IF NOT EXISTS ${DATABASE_NAME}"
 mysql ${mysqlauth} ${DATABASE_NAME} < ezpublish_legacy/var/ezdemo_site/dump.sql
